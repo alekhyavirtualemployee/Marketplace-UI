c80e86bad65954b461213f78f9ed3055e489a45c
Jitender Tanwar <jitendertanwar@virtualemployee.com>
2022-05-04 15:20:52 +0530

    first commit

---
+++ src/components/navigation.jsx
@@ -0,0 +1,72 @@
+export const Navigation = (props) => {
+  return (
+    <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
+      <div className='navbar-header'>
+      <img src="img/logo-placeholder-image.png" className="logo-responsive" alt="logo" />
+        </div>
+      <div className='container'>
+      
+        <div className='navbar-header'>
+          <button
+            type='button'
+            className='navbar-toggle collapsed'
+            data-toggle='collapse'
+            data-target='#bs-example-navbar-collapse-1'
+          >
+            {' '}
+            <span className='sr-only'>Toggle navigation</span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+          </button>
+          <a className='navbar-brand page-scroll left-heading' href='#page-top'>
+            NFT Market place
+          </a>{' '}
+        </div>
+
+        <div
+          className='collapse navbar-collapse'
+          id='bs-example-navbar-collapse-1'
+        >
+          <ul className='nav navbar-nav navbar-right'>
+            <li>
+              <a href='#features' className='page-scroll'>
+                Features
+              </a>
+            </li>
+            <li>
+              <a href='#about' className='page-scroll'>
+                About
+              </a>
+            </li>
+            <li>
+              <a href='#services' className='page-scroll'>
+                Services
+              </a>
+            </li>
+            <li>
+              <a href='#portfolio' className='page-scroll'>
+                STARS
+              </a>
+            </li>
+            <li>
+              <a href='#testimonials' className='page-scroll'>
+                Testimonials
+              </a>
+            </li>
+            <li>
+              <a href='#create' className='page-scroll'>
+                Create
+              </a>
+            </li>
+            <li>
+              <a href='#contact' className='page-scroll'>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
