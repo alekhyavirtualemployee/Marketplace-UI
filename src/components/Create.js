d8e515160bf9964ef391b6d9a17815f290aeb8ef
Farzina Sahin <farzinasahin@virtualemployee.com>
2022-05-09 16:05:58 +0530

    change

---
+++ src/components/Create.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Header, Rightmenu, Leftmenu } from "./Layout";
+
+const Create = ()=>{
+    return(
+        <div>Create</div>
+    );
+}
+
+export default Create
